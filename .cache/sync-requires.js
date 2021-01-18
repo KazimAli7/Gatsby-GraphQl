b9d91390afa57115cc8c775f1ba6c82032b3e194
KazimAli7 <kazim.agha@invozone.com>
2021-01-18 16:33:02 +0500

    added new routes for new pages

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,10 +8,11 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/About.js"))),
+  "component---src-pages-api-call-js": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/ApiCall.js"))),
   "component---src-pages-blog-view-tsx": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/BlogView.tsx"))),
   "component---src-pages-home-tsx": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/home.tsx"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/index.js"))),
-  "component---src-pages-post-view-js": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/PostView.js"))),
+  "component---src-pages-post-view-tsx": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/PostView.tsx"))),
   "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/pages/using-typescript.tsx"))),
   "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/kazim-invozone/Documents/WorkPlace/GitProject/Gatsby-GraphQl/src/templates/blog-post.js")))
 }
